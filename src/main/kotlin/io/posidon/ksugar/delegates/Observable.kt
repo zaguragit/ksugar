58c5de7077d872137555cea576f78f78f01f9b8e
Zagura <zagura@posidon.io>
2022-04-11 14:51:51 +0200

    stuff yet again

--- src/main/kotlin/io/posidon/ksugar/delegates/Observable.kt
+++ src/main/kotlin/io/posidon/ksugar/delegates/Observable.kt
@@ -5,26 +5,26 @@ import kotlin.properties.ObservableProperty
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-inline fun <T : Any?> Delegates.observable(crossinline onChange: (property: KProperty<*>, oldValue: T?, newValue: T?) -> Unit): ReadWriteProperty<Any?, T?> =
+inline fun <T> Delegates.observable(crossinline onChange: (property: KProperty<*>, oldValue: T?, newValue: T?) -> Unit): ReadWriteProperty<Any?, T?> =
     observable(null, onChange)
 
 
 inline fun <T> Delegates.observable(initialValue: T, crossinline onChange: (oldValue: T, newValue: T) -> Unit): ReadWriteProperty<Any?, T> =
     observable(initialValue) { _, old, new -> onChange(old, new) }
 
-inline fun <T : Any?> Delegates.observable(crossinline onChange: (oldValue: T?, newValue: T?) -> Unit): ReadWriteProperty<Any?, T?> =
+inline fun <T> Delegates.observable(crossinline onChange: (oldValue: T?, newValue: T?) -> Unit): ReadWriteProperty<Any?, T?> =
     observable { _, old, new -> onChange(old, new) }
 
 
 inline fun <T> Delegates.observable(initialValue: T, crossinline onChange: (value: T) -> Unit): ReadWriteProperty<Any?, T> =
     observable(initialValue) { _, _, new -> onChange(new) }
 
-inline fun <T : Any?> Delegates.observable(crossinline onChange: (value: T?) -> Unit): ReadWriteProperty<Any?, T?> =
+inline fun <T> Delegates.observable(crossinline onChange: (value: T?) -> Unit): ReadWriteProperty<Any?, T?> =
     observable { _, _, new -> onChange(new) }
 
 
 inline fun <T> Delegates.observable(initialValue: T, crossinline onChange: () -> Unit): ReadWriteProperty<Any?, T> =
     observable(initialValue) { _, _, _ -> onChange() }
 
-inline fun <T : Any?> Delegates.observable(crossinline onChange: () -> Unit): ReadWriteProperty<Any?, T?> =
+inline fun <T> Delegates.observable(crossinline onChange: () -> Unit): ReadWriteProperty<Any?, T?> =
     observable { _, _, _ -> onChange() }
--- src/main/kotlin/io/posidon/ksugar/delegates/Vetoable.kt
+++ src/main/kotlin/io/posidon/ksugar/delegates/Vetoable.kt
@@ -5,26 +5,26 @@ import kotlin.properties.ObservableProperty
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-inline fun <T : Any?> Delegates.vetoable(crossinline onChange: (property: KProperty<*>, oldValue: T?, newValue: T?) -> Boolean): ReadWriteProperty<Any?, T?> =
+inline fun <T> Delegates.vetoable(crossinline onChange: (property: KProperty<*>, oldValue: T?, newValue: T?) -> Boolean): ReadWriteProperty<Any?, T?> =
     vetoable(null, onChange)
 
 
 inline fun <T> Delegates.vetoable(initialValue: T, crossinline onChange: (oldValue: T, newValue: T) -> Boolean): ReadWriteProperty<Any?, T> =
     vetoable(initialValue) { _, old, new -> onChange(old, new) }
 
-inline fun <T : Any?> Delegates.vetoable(crossinline onChange: (oldValue: T?, newValue: T?) -> Boolean): ReadWriteProperty<Any?, T?> =
+inline fun <T> Delegates.vetoable(crossinline onChange: (oldValue: T?, newValue: T?) -> Boolean): ReadWriteProperty<Any?, T?> =
     vetoable { _, old, new -> onChange(old, new) }
 
 
 inline fun <T> Delegates.vetoable(initialValue: T, crossinline onChange: (value: T) -> Boolean): ReadWriteProperty<Any?, T> =
     vetoable(initialValue) { _, _, new -> onChange(new) }
 
-inline fun <T : Any?> Delegates.vetoable(crossinline onChange: (value: T?) -> Boolean): ReadWriteProperty<Any?, T?> =
+inline fun <T> Delegates.vetoable(crossinline onChange: (value: T?) -> Boolean): ReadWriteProperty<Any?, T?> =
     vetoable { _, _, new -> onChange(new) }
 
 
 inline fun <T> Delegates.vetoable(initialValue: T, crossinline onChange: () -> Boolean): ReadWriteProperty<Any?, T> =
     vetoable(initialValue) { _, _, _ -> onChange() }
 
-inline fun <T : Any?> Delegates.vetoable(crossinline onChange: () -> Boolean): ReadWriteProperty<Any?, T?> =
+inline fun <T> Delegates.vetoable(crossinline onChange: () -> Boolean): ReadWriteProperty<Any?, T?> =
     vetoable { _, _, _ -> onChange() }
